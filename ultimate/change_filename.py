785baf827c7d7433314d8f9df206d5179415410b
Matt Tellander <dawndimension@gmail.com>
2022-07-08 03:31:20 -0500

    add select random files and copy them to upload dir feature to change_filename script

--- ultimate/change_filename.py
+++ ultimate/change_filename.py
@@ -4,6 +4,7 @@ from pickle import FALSE, TRUE
 import random
 from Generate_Title import *
 import glob
+import shutil
 
 # importing all the
 # functions defined in test.py
@@ -29,21 +30,24 @@ import glob
 
 #can be a separate product from viral title pro. Will have it's own simple GUI and will be a free bonus software
 
-staging_dir="C:\\Users\\Brandon\\Desktop\\MINIGOLFUS\\exports\\_STAGING\\experiment-616"
-#staging_dir="D:\\carmanjaro\\renamer"
+video_count = 14
+copy_from_dir="C:\\Users\\Brandon\\Desktop\\MINIGOLFUS\\exports\\_STAGING\\Viral_Title_Pro_Uploads"
+#careful what you set upload dir as. Files are deleted from upload dir so DO NO set this to location where you store videos!!
+upload_dir="C:\\Users\\Brandon\\Desktop\\MINIGOLFUS\\exports\\_STAGING\\experiment-616"
+#upload_dir="D:\\carmanjaro\\renamer"
 file_list = ""
-subject = "golf shot"
+subject = "putt putt video"
 titles = []
 tags = "#shorts #minigolf #golf"
 file_extension = ".mp4"
 
-def get_files(staging_dir):       # 1.Get file names from directory
-    #file_list=os.listdir(staging_dir)
+def get_files(upload_dir):       # 1.Get file names from directory
+    #file_list=os.listdir(upload_dir)
     #print ("Files in selected directory: "+str(file_list))
     
     # Get list of all files in a given directory sorted by name
-    file_list = sorted( filter( lambda x: os.path.isfile(os.path.join(staging_dir, x)),
-                        os.listdir(staging_dir) ) )
+    file_list = sorted( filter( lambda x: os.path.isfile(os.path.join(upload_dir, x)),
+                        os.listdir(upload_dir) ) )
     for file_name in file_list:
       print(file_name)
     
@@ -63,10 +67,10 @@ def generate_filename_handler(subject,tags):
   return filename
 
 
-def rename_files_handler(files, staging_dir):
-  os.chdir(staging_dir)
+def rename_files_handler(files, upload_dir):
+  os.chdir(upload_dir)
 
-  print("Looping through current files in directory: "+staging_dir)
+  print("Looping through current files in directory: "+upload_dir)
   for index, old_filename in enumerate(files):
     print (index, ",",old_filename)
     print("current filename:"+files[index])
@@ -100,9 +104,48 @@ def rename_files_handler(files, staging_dir):
   return True
     
 
+def delete_files_in_upload_dir(upload_dir):
+  for f in os.listdir(upload_dir):
+    os.remove(os.path.join(upload_dir, f))
+  print("Files deleted from "+str(upload_dir))  
 
-file_list = get_files(staging_dir)
-Success = rename_files_handler(file_list, staging_dir)
+def copy_video_files_for_upload(files_in_dir, video_count, src_folder, dst_folder):
+    #loop, at random based on video count, select videos that will be uploaded, 
+    #copy files over to upload dir
+    # also call isVideo(files[index])  only attempt to upload video files
+    #shuffle list of files, grab last one, then discard it from list after use it using pop. this way no duplicates per upload.
+    print("Listing files in source video folder before copy...")
+    for x in range(video_count):
+        print("files in dir before shuffle "+str(files_in_dir))
+        random.shuffle(files_in_dir)
+        print("files in dir after shuffle "+str(files_in_dir))
+        
+        # file names
+        file = str(files_in_dir[-1])
+        src_file = src_folder + "\\" + file
+        dst_file = dst_folder + "\\" + file
+
+        shutil.copyfile(src_file, dst_file)
+        print('Copied')     
+        
+        files_in_dir.pop()
+        print("files in dir after pop "+str(files_in_dir))
+    print("===================")
+    print("done copying files to upload directory...")
+
+    return True
+
+
+
+#optional - copy files randomly from videos directory into this directory
+#delete files in upload dir before copying new ones over. the folder is a temporary one. 
+#can also have it create a new folder with timestamp but that would use up space
+delete_files_in_upload_dir(upload_dir)
+files_in_dir = get_files(copy_from_dir)
+Success = copy_video_files_for_upload(files_in_dir, video_count, copy_from_dir, upload_dir)
+
+file_list = get_files(upload_dir)
+Success = rename_files_handler(file_list, upload_dir)
 
 
 
